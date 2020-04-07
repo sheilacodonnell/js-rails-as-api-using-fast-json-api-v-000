6dd987e6eb3373b3d484fd8dc0d640658f778da0
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-07 14:34:31 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -7,6 +7,6 @@ class SightingsController < ApplicationController
   def show
     sighting = Sighting.find(params[:id])
     options = { include: [:bird, :location] }
-    render json: SightingSerializer.new(sighting)
+    render json: SightingSerializer.new(sighting, options)
   end
 end
